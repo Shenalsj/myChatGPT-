feb9599694c5414ccc5c072c388f5971037d9eec
shenalsj <shenal.sj@gmail.com>
2023-04-10 00:35:29 +0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) =>{
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:3001',{
+  const response = await fetch('https://chatapp-6cll.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
